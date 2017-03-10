4836017318a603e11f87429b5db7dcfa72da2f31
jprieto <jprieto@hotelbeds.com>
2017-03-10 20:22:25 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hb-techtalk-spinnaker
+# Hotelbeds TechTalk Spinnaker Demo
 Application to be deployed in the Hotelbeds TechTalk about Spinnaker
+
+## Installation
+```
+$ git clone git://github.com/jlprietopriego/hb-techtalk-spinnaker.git
+$ gradlew clean build
+$ gradlew clean build buildRpm
+```
---
+++ src/test/java/hbg/jprieto/spinnakerdemo/ApplicationTests.java
@@ -0,0 +1,31 @@
+package hbg.jprieto.spinnakerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import hbg.jprieto.spinnakerdemo.Application;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest(randomPort = true)
+public class ApplicationTests {
+
+    private final RestTemplate restTemplate = new TestRestTemplate();
+
+    @Value("${local.server.port}")
+    private int port;
+
+    @Test
+    public void testHelloWorld() {
+        final String response = restTemplate.getForObject("http://localhost:" + port, String.class);
+        assertEquals("Jose says Welcome to the Spinnaker Demo!!", response);
+    }
+}
